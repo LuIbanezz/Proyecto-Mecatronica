f743a17c386f463742b3a5237942d4f27bdcffad
agalan203 <83973836+agalan203@users.noreply.github.com>
2024-11-07 17:25:01 -0300

    Encoders funcionando

--- Prueba_motores.ino
+++ Prueba_motores.ino
@@ -1,30 +1,31 @@
 /*** Timer 1 setup for Sample ***/
 #include <TimerOne.h>
-#include <MPU9250.h>
 #include <Wire.h> 
-
-MPU9250 mpu;
 
 const unsigned long interval = 10000; // intervalo en us
 
 /*** Motor Setup ***/
-const int enA = 6;
-const int in1 = 4;
-const int in2 = 2;
+const int enA = 8;
+const int in1 = 28;
+const int in2 = 30;
 
-const int enB = 5;
-const int in3 = 7;
-const int in4 = 8;
+const int enB = 9;
+const int in3 = 29;
+const int in4 = 31;
 
 /*** Sensor Setup ***/
-const int sens1= 14;
-const int sens2= 15;
-const int sens3= 16;
-const int sens4= 17;
-const int sens5= 3; 
-const int sens6= 12;
-const int sens7= 10; 
-const int sens8= 9; 
+const int sens1= 47;
+const int sens2= 23;
+const int sens3= 52;
+const int sens4= 50;
+const int sens5= 53; 
+const int sens6= 43;
+const int sens7= 51; 
+const int sens8= 45;
+
+/*** Encoder Setup ***/
+const int encoderA= 3;
+const int encoderB= 2;
 
 
 #define DELAY 2000
@@ -35,23 +36,15 @@ void setup() {
   delay(2000);
   Serial.println("Serial Open");
 
-  mpu.setup(0x68);
-  delay(5000);
-
-  mpu.calibrateAccelGyro();
-  mpu.calibrateMag();
-
   // Motor Setup
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-
   pinMode(enB, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
   // Sensor setup
-
   pinMode(sens1, INPUT);
   pinMode(sens2, INPUT);
   pinMode(sens3, INPUT);
@@ -61,8 +54,6 @@ void setup() {
   pinMode(sens7, INPUT);
   pinMode(sens8, INPUT);
 
-  //Timer1.initialize(interval);   //  conversión a us
-  //Timer1.attachInterrupt(timerCallback);
 
   delay(1000);
 
@@ -70,20 +61,17 @@ void setup() {
 
 void loop() {
 
- // int v = analogRead(sens5);
-
-  //Serial.println(v);
-
   if(!digitalRead(sens8)) //si detecta mesa
   {
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
       analogWrite(enA, 255);
-      Serial.println("detecta mesa");
 
       digitalWrite(in3, LOW);
       digitalWrite(in4, HIGH);
       analogWrite(enB, 255);
+
+      Serial.println("detecta mesa");
   }
 
   else
@@ -91,17 +79,12 @@ void loop() {
       digitalWrite(in1, LOW);
       digitalWrite(in2, LOW);
       analogWrite(enA, 0);
-      Serial.println("no detecta mesa");
 
       digitalWrite(in3, LOW);
       digitalWrite(in4, LOW);
       analogWrite(enB, 0);
-  }
-  
-  if (mpu.update()){
-    Serial.print(mpu.getYaw()); Serial.print(",");
-    Serial.print(mpu.getPitch()); Serial.print(",");
-    Serial.print(mpu.getRoll());Serial.print("\n");
+
+      Serial.println("no detecta mesa");
   }
 }
 
--- prueba_encoder.ino
+++ prueba_encoder.ino
@@ -1,25 +1,39 @@
 /*** Timer 1 setup for Sample ***/
 #include <TimerOne.h>
-#include <MPU9250.h>
 #include <Wire.h> 
 
 #define DELAY 2000
 
-MPU9250 mpu;
-
-
-
 /*** Motor Setup ***/
-const int enA = 6;
-const int in1 = 4;
-const int in2 = 2;
+const int enA = 8;
+const int in1 = 28;
+const int in2 = 30;  //Motor A aprox 3060 pulsos en 2 seg
+
+const int enB = 9;
+const int in3 = 29;
+const int in4 = 31; //Motor B aprox 2370 pulsos en 2 seg (varia bastante :C)
+
+/*** Sensor Setup ***/
+const int sens1= 47;
+const int sens2= 23;
+const int sens3= 52;
+const int sens4= 50;
+const int sens5= 53; 
+const int sens6= 43;
+const int sens7= 51; 
+const int sens8= 45;
+
+/*** Encoder Setup ***/
+const int encoderA= 2;
+const int encoderB= 3;
 
 /*** Encoder Setup***/
-//static int pulseCount = 0; // Contador de pulsos
 unsigned long previousMillis = 0; // Tiempo de la última medición de velocidad
 const int encoderPin = 13; // Pin para el Canal A del encoder
 const int interval = 1000; // Intervalo de tiempo para calcular la velocidad (ms)
 
+volatile int pulseCountA = 0; // Contador de pulsos del encoder A
+volatile int pulseCountB = 0; // Contador de pulsos del encoder B
 
 
 void setup() {
@@ -35,25 +49,49 @@ void setup() {
   pinMode(in2, OUTPUT);
 
   // Encoder Setup
-  pinMode(encoderPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(encoderPin), countPulse, RISING); // Interrupción en flanco ascendente
+  interrupts();
+  attachInterrupt(digitalPinToInterrupt(encoderA), countPulseA, RISING); // Interrupción en flanco ascendente
+  attachInterrupt(digitalPinToInterrupt(encoderB), countPulseB, RISING); // Interrupción en flanco ascendente
 
 }
 
 void loop() {
     // Mostrar el número total de pulsos en el monitor serie
-    Serial.println("Pulsos: ");
-    Serial.println(countPulse());
-    //delay(500); // Actualización cada medio segundo
 
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     analogWrite(enA, 120);
 
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    analogWrite(enB, 120);
+
+    delay(2000);
+
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    analogWrite(enA, 120);
+
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 120);
+
+    Serial.println("Pulsos A: ");
+    Serial.println(pulseCountA);
+    Serial.println("Pulsos B: ");
+    Serial.println(pulseCountB);
+    
+    delay(2000);
+
+    pulseCountA = 0;
+    pulseCountB = 0;
+
 }
 
-int countPulse() {
-  static int pulseCount = 0; 
-   
-    return pulseCount++; // Incrementa el contador de pulsos cada vez que detecta un flanco ascendente
+void countPulseA() {
+    pulseCountA = pulseCountA+1; // Incrementa el contador de pulsos cada vez que detecta un flanco ascendente
+}
+
+void countPulseB() {
+    pulseCountB = pulseCountB+1; // Incrementa el contador de pulsos cada vez que detecta un flanco ascendente
 }
